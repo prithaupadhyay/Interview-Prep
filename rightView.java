35fbd0f87b113069543a7a557debae3d19d29bf7
Ishita Das <ishi0490@gmail.com>
2019-10-12 20:28:10 +0530

    right view of binary tree

---
+++ rightView.java
@@ -0,0 +1,23 @@
+class Tree{
+    int max = 0;
+    Node root;
+    void rightViewUtil(Node node, int level){
+    
+    if(node == null){
+        return;
+    }
+    if(max<level){
+        //System.out.print(level+ " "+max);
+        System.out.print(node.data + " ");
+        max=level;
+    }
+    rightViewUtil(node.right,level+1);
+    rightViewUtil(node.left,level+1);
+}
+void rightView(Node root)
+{
+   // Your Code here
+   rightViewUtil(root,1);
+}
+
+}
