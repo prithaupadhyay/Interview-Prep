0e3bdb82a3ba5dcb17de3ba3e4f455a8b85c4906
violentdelight <mohakkhare08@gmail.com>
2019-10-27 14:48:07 +0530

    Add abcString.cpp

---
+++ abcString.cpp
@@ -0,0 +1,18 @@
+/* Given a length n.
+ Count the number of strings of length n that can be made using ‘a’, ‘b’ and ‘c’ 
+ with at-most one ‘b’ and two ‘c’s allowed. */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long int t;
+	cin >> t;
+	while (t--){
+	    long long int n;
+	    cin >> n;
+	    long long int ans = (1 + 2*n + (pow(n, 3)-n)/2);
+	    cout <<  ans << endl;
+	}
+	return 0;
+}
